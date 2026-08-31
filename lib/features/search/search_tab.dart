240a6cbad8c5a0a4877dc18a845299c846fd54f2
君に歌を <57479390+tsuyuAme@users.noreply.github.com>
2026-08-31 22:55:51 +0800

    Add files via upload

--- lib/features/search/search_tab.dart
+++ lib/features/search/search_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:venera_next/foundation/app.dart';
 import 'package:venera_next/foundation/app_page_route.dart';
 import 'package:venera_next/features/search/search_page.dart';
 
@@ -20,12 +21,34 @@ class SearchTabState extends State<SearchTab>
   @override
   bool get wantKeepAlive => true;
 
+  @override
+  void initState() {
+    super.initState();
+    // Register for mouse-back / Esc via [App.pop]
+    App.secondaryNavigatorKey = navigatorKey;
+  }
+
+  @override
+  void dispose() {
+    if (App.secondaryNavigatorKey == navigatorKey) {
+      App.secondaryNavigatorKey = null;
+      App.secondaryNavigatorActive = false;
+    }
+    super.dispose();
+  }
+
   /// Pop nested routes back to the search form (optional when re-tapping tab).
   void popToRoot() {
     navigatorKey.currentState?.popUntil((route) => route.isFirst);
   }
 
   bool get canPop => navigatorKey.currentState?.canPop() ?? false;
+
+  void pop() {
+    if (canPop) {
+      navigatorKey.currentState!.pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
